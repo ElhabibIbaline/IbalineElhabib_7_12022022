6521f7cd4f896cb9b817dcfac170044daa2b2fc8
elhabib ibaline <h.ibaline@gmail.com>
2022-06-09 08:38:36 +0100

    Installer&Executer Express sur le serveur Node

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la 3eme réponse du serveur !');
-});
+app.set('port', process.env.PORT || 5000);
+const server = http.createServer(app);
 
-server.listen(process.env.PORT || 5001);
+server.listen(process.env.PORT || 5000);
