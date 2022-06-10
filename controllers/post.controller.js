32c709ba4f418a03856c73875db85d85959c1f43
elhabib ibaline <h.ibaline@gmail.com>
2022-06-10 12:42:47 +0100

    Implimenter updatePost

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,7 +1,8 @@
 const postModel = require("../models/post.model");
+const ObjectID = require("mongoose").Types.ObjectId;
 
 module.exports.readPost = (req, res) => {
-  PostModel.find((err, docs) => {
+  postModel.find((err, docs) => {
     if (!err) res.send(docs);
     else console.log("Error to get data : " + err);
   });
@@ -22,4 +23,23 @@ module.exports.createPost = async (req, res) => {
   } catch (err) {
     return res.status(400).send(err);
   }
+};
+
+module.exports.updatePost = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  const updatedRecord = {
+    message: req.body.message,
+  };
+
+  postModel.findByIdAndUpdate(
+    req.params.id,
+    { $set: updatedRecord },
+    { new: true },
+    (err, docs) => {
+      if (!err) res.send(docs);
+      else console.log("Update error : " + err);
+    }
+  );
 };
